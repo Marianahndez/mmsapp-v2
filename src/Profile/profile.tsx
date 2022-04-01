fd3d946c914a41c2fd1b9473de115ad419d08859
Mariana Zamarripa <marianahndez@Marianas-MacBook-Air.local>
2022-03-31 19:16:21 -0600

    eslint updates

--- src/Profile/profile.tsx
+++ src/Profile/profile.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useState, useContext } from 'react';
