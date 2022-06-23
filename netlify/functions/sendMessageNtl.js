4afff536dc8cc1839f5c361e5100079c1cc763e7
Mariana Zamarripa <marianahndez@Marianas-MacBook-Air.local>
2022-06-23 14:14:31 -0500

    msg service updated v3

--- netlify/functions/sendMessageNtl.js
+++ netlify/functions/sendMessageNtl.js
@@ -1,16 +1,17 @@
+/* eslint-disable global-require */
 /* eslint-disable func-names */
 /* eslint-disable max-len */
 /* eslint-disable no-return-await */
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable indent */
-require('dotenv');
-const client = require('twilio')(
-  process.env.REACT_APP_TWILIO_ACCOUNT_SID,
-  process.env.REACT_APP_TWILIO_AUTH_TOKEN,
-);
 
 exports.handler = async function (event, context) {
+  const accountSid = process.env.REACT_APP_TWILIO_ACCOUNT_SID;
+  const authToken = process.env.REACT_APP_TWILIO_AUTH_TOKEN;
+  const client = require('twilio')(accountSid, authToken);
   const eventTo = JSON.parse(event.body);
+  console.log('event tkn: ', authToken);
+  console.log('event sid: ', accountSid);
   console.log('event: ', eventTo);
   client.messages.create({
     from: process.env.REACT_APP_TWILIO_PHONE_NUMBER,
